46d21c467497fe9a00dd5c96ea1a549071497fbe
Sven Bouwhuis <sven.bouwhuis@hotmail.com>
2022-07-06 10:41:45 +0200

    try to trigger pipeline

--- keystone/keystone.ts
+++ keystone/keystone.ts
@@ -14,6 +14,8 @@ import { lists } from './schema';
 // Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
 import { withAuth, session } from './auth';
 
+//Small test comment to hopefully trigger pipeline
+
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
