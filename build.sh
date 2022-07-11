eac586232a1b84a9027a8cf8b6f664e3e99bd99a
t-theunissen-moovd <78902142+t-theunissen-moovd@users.noreply.github.com>
2022-07-11 21:15:38 +0200

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $CONNECTION_STRING_DEV
-echo "$CONNECTION_STRING_DEV"
-cat $CONNECTION_STRING_DEV > ".env"
+echo $CONNECTION_STRING_DEV &&
+echo "$CONNECTION_STRING_DEV" &&
+cat $CONNECTION_STRING_DEV > ".env" &&
 cat $CONNECTION_STRING_DEV > "keystone/.env"
